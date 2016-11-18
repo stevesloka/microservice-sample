c34a8e4c4bc56fcc62ace9d23cbff6fdc9d8264c
Steve Sloka <slokas@upmc.edu>
2016-11-18 15:06:09 -0500

    Formatted request better

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, os.Getenv("SERVICE"))
+		fmt.Fprintf(w, fmt.Sprintf("Service: %s \nRequest Time: %s", os.Getenv("SERVICE"), time.Now().Format("Mon Jan _2 15:04:05 2006")))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
